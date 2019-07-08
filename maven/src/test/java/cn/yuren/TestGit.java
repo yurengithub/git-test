56e982f155b82ccacab1bfd5e996555724b44d10
lisi <lisi@yuren.cn>
2019-07-08 11:57:18 +0800

    新增一个方法

--- maven/src/test/java/cn/yuren/TestGit.java
+++ maven/src/test/java/cn/yuren/TestGit.java
@@ -7,4 +7,9 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("测试git版本控制工具");
     }
+
+    public static TestGit getInstance() {
+        System.out.println("dfdf");
+        return null;
+    }
 }
