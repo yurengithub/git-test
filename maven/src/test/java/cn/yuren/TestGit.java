424e6a16c464bb450d1837c25752897e1c869dc1
lisi <lisi@yuren.cn>
2019-07-08 11:51:27 +0800

    将本地工程添加到git

---
+++ maven/src/test/java/cn/yuren/TestGit.java
@@ -0,0 +1,10 @@
+package cn.yuren;
+
+/**
+ * Created by Administrator on 19-7-8.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git版本控制工具");
+    }
+}
